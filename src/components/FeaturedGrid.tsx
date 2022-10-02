930d157c6309ae74d158857328af82c60ffc504d
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-02 23:51:10 +0800

    Create FeaturedGrid component

---
+++ src/components/FeaturedGrid.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { FeaturedItem } from "../types";
+import { Card } from "./";
+import { featured } from "../data/sample.js";
+
+const FeaturedGrid = () => {
+    const items : FeaturedItem[] = featured;
+
+    return (
+        <div className="max-w-[1640px] mx-auto grid md:grid-cols-3 gap-6 p-4">
+            {
+                items.map(item => 
+                    <Card 
+                        key={item.id}
+                        title={item.title} 
+                        subtitle={item.subtitle} 
+                        imageUrl={item.image} 
+                        ctaText="Order Now" /> 
+                )
+            }
+        </div>
+    );
+}
+
+export default FeaturedGrid;
