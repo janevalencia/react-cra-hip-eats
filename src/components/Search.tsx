b034a26d3dc2dc21c66908da436f43533afebe32
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-02 15:42:53 +0800

    Update SearchBar width sizing breakpoint

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,7 +3,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 const Search = () => {
     return (
-        <div className="flex items-center gap-x-2 px-2 w-[200px] sm:w-[400px] lg:w-[500px] bg-gray-200 rounded-full">
+        <div className="flex items-center gap-x-2 px-2 w-[200px] sm:w-[350px] lg:w-[500px] bg-gray-200 rounded-full">
             <AiOutlineSearch size={25} />
             <input 
                 className="bg-transparent p-2 w-full rounded-full focus:outline-none"
