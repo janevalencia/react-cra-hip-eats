af6eac34f2ac8870a73a06e3ea6ab2708169cd2e
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-02 15:08:08 +0800

    Create a SearchBar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 // Import all components into a single-file for easy management.
-export {default as Navbar} from "./Navbar.tsx";
-export {default as Layout} from "./Layout.tsx";
+export {default as Search} from "./Search";
+export {default as Navbar} from "./Navbar";
+export {default as Layout} from "./Layout";
