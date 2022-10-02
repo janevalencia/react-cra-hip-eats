ff53d51b2efe8ebb809fb0c5f32cbfdb9399162f
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-02 17:19:54 +0800

    Add all created components to index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 // Import all components into a single-file for easy management.
 export {default as Search} from "./Search";
-export {default as Navbar} from "./Navbar";
+export {default as Sidebar} from "./Sidebar";
+export {default as Portals} from "./Portals";
 export {default as Layout} from "./Layout";
