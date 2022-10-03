554ef883efaa7f3678711767aa42c5af4fabc6c9
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-03 23:31:36 +0800

    Add button hover effect to Card component

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -17,7 +17,7 @@ const Card = ({title, subtitle, ctaText, imageUrl} : CardProps) => {
                 <p className='font-bold text-xl px-3 pt-4'>{title}</p>
                 <p className='px-3'>{subtitle}</p>
                 <button 
-                    className='absolute border-white bg-white text-black mx-3 bottom-4 rounded-full py-2'
+                    className='absolute border-white bg-white text-black mx-3 bottom-4 rounded-full py-2 hover:bg-transparent hover:text-white duration-500'
                 >
                     {ctaText}
                 </button>
