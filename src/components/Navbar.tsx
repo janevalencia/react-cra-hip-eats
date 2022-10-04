f9b4ab2200eade7a34c141a7ede43164127e33d4
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-04 15:50:10 +0800

    Update the cart button to display no of cart items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { AiOutlineMenu } from "react-icons/ai";
 import { BsFillCartFill } from "react-icons/bs";
 import { Link } from "react-router-dom";
@@ -17,6 +17,18 @@ const defaultBrandProp = {
 const Navbar = ({brand} : typeof defaultBrandProp) => {
   // Manage Sidebar menu state.
   const [showNav, setShowNav] = useState<boolean>(false);
+
+  // Manage Cart amount.
+  const [cartNo, setCartNo] = useState<number>(0);
+
+  // Fetch Cart amount from localStorage if any.
+  useEffect(() => {
+    let storage = localStorage.getItem('orders');
+    if (storage) {
+      const orders : string[] = JSON.parse(storage);
+      setCartNo(orders.length);
+    }
+  }, [cartNo]);
 
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
@@ -42,9 +54,10 @@ const Navbar = ({brand} : typeof defaultBrandProp) => {
       {/* Cart Menu */}
       <div className="bg-black text-white rounded-full py-2">
         <Link to={`/orders`}>
-          <button className="flex items-center gap-x-2 border-none">
+          <button className="flex items-center gap-x-1 border-none">
             <BsFillCartFill />
-            <span className="hidden lg:block">Cart</span>
+            <span className="hidden lg:inline">Cart</span>
+            {cartNo > 0 && <span>{`(${cartNo})`}</span>}
           </button>
         </Link>
       </div>
