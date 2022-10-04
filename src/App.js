17f272d6e012349cd3d22978f0c4432fd6cccec4
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-04 14:22:34 +0800

    Add router and define routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import { Hero, FeaturedGrid, FoodList, Deals } from "./components";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Orders from "./pages/Orders";
 
 function App() {
   return (
-    <div className="App">
-      <Hero />
-      <FeaturedGrid />
-      <FoodList />
-      <Deals />
+    <div className="min-h-[35vh]">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/orders" element={<Orders />} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { Layout } from './components';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Layout } from "./components";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Layout>
-      <App />
-    </Layout>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <Layout>
+        <App />
+      </Layout>
+    </React.StrictMode>
+  </BrowserRouter>
 );
