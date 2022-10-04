661b9c59689bada112f7f5cd6cd820cece46e190
Jane Valencia <31334333+janevalencia@users.noreply.github.com>
2022-10-04 15:49:56 +0800

    Update Card component so the OrderNow btn save item to storage

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -10,6 +10,34 @@ type CardProps = {
 
 // Render Card component.
 const Card = ({title, subtitle, ctaText, imageUrl} : CardProps) => {
+    
+    // Save item to localStorage to be displayed on Cart.
+    const addToCart = () => {
+        let storage = localStorage.getItem('orders');
+        if (storage) {
+            // Parsing the localStorage into JS Object.
+            let storedCart : string[] = JSON.parse(storage);
+
+            // Push new item to storedCart.
+            storedCart.push(title);
+
+            // Save back the localStorage.
+            localStorage.setItem('orders', JSON.stringify(storedCart));
+        } else {
+            // Create new localStorage item called orders.
+            let orders : string[] = [];
+
+            // Push new item to storedCart.
+            orders.push(title);
+
+            // Save back the localStorage.
+            localStorage.setItem('orders', JSON.stringify(orders));
+        }
+
+        window.location.reload();
+    }
+
+    // Render.
     return (
         <div className='rounded-xl relative'>
             {/* Overlay */}
@@ -17,6 +45,7 @@ const Card = ({title, subtitle, ctaText, imageUrl} : CardProps) => {
                 <p className='font-bold text-xl px-3 pt-4'>{title}</p>
                 <p className='px-3'>{subtitle}</p>
                 <button 
+                    onClick={addToCart}
                     className='absolute border-white bg-white text-black mx-3 bottom-4 rounded-full py-2 hover:bg-transparent hover:text-white duration-500'
                 >
                     {ctaText}
